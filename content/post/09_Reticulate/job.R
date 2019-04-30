071274ea6640005f6d2fe216e79fc85420422e2b
DannyJRa <danny_zurich@outlook.com>
2019-04-30 13:13:19 +0200

    commit blog 09 including files

--- content/post/09_Reticulate/job.R
+++
@@ -1,7 +0,0 @@
-#Test
-library(reticulate)
-source_python("flights.py")
-flights <- read_flights("flights.csv")
-
-library(ggplot2)
-ggplot(flights, aes(carrier, arr_delay)) + geom_point() + geom_jitter()
